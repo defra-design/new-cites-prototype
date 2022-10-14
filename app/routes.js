6eff89e937b13ad65f1935df2d82b9cf1f3fd109
Richard O'Dwyer <richard.odwyer@defra.gov.uk>
2022-10-14 12:00:39 +0100

    Contact details flow updates

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,16 @@ if (req.session.data['isAgent'] == 'yes') {
 }
 })
 
+// Confirm applicants international address
+router.post('/v7/confirm-applicants-international-address', function (req, res) {
+  res.redirect('what-is-the-name-of-the-species')
+})
+
+// Confirm delivery address
+router.post('/v7/confirm-delivery-address', function (req, res) {
+  res.redirect('what-is-the-name-of-the-species')
+})
+
 // Confirm your address agent
 router.post('/v7/confirm-your-address-agent', function (req, res) {
   res.redirect('enter-your-contact-details-agent-led')
@@ -17,13 +27,47 @@ router.post('/v7/confirm-your-address-agent', function (req, res) {
 
 // Confirm your address agent led
 router.post('/v7/confirm-your-address-agent-led', function (req, res) {
-  res.redirect('species-add-to-list-0')
+  if (req.session.data['delivery-address'] == 'delivery-address-3') {
+    res.redirect('enter-delivery-address') }
+    else {
+    res.redirect('what-is-the-name-of-the-species')
+  }
 })
 
 // Confirm your address applicant
 router.post('/v7/confirm-your-address-applicant', function (req, res) {
-  res.redirect('species-add-to-list-0')
+  if (req.session.data['delivery-address-applicant'] == 'delivery-address-2-applicant') {
+    res.redirect('enter-delivery-address') }
+    else {
+    res.redirect('what-is-the-name-of-the-species')
+  }
 })
+
+// Enter an international address
+router.post('/enter-an-international-address', function (req, res) {
+  res.redirect('confirm-applicants-international-address')
+})
+
+// Enter delivery address
+router.post('/enter-delivery-address', function (req, res) {
+  res.redirect('confirm-delivery-address')
+})
+
+// Enter your address manually - agent
+router.post('/enter-your-address-manually-agent', function (req, res) {
+  res.redirect('confirm-your-address-agent')
+})
+
+// Enter your address manually - agent led
+router.post('/enter-your-address-manually-agent-led', function (req, res) {
+  res.redirect('confirm-your-address-agent-led')
+})
+
+// Enter your address manually - applicant
+router.post('/enter-your-address-manually-applicant', function (req, res) {
+  res.redirect('confirm-your-address-applicant')
+})
+
 
 // Enter your contact details agent
 router.post('/v7/enter-your-contact-details-agent', function (req, res) {
@@ -53,6 +97,11 @@ router.post('/v7/select-your-address-agent', function (req, res) {
 // Select your address applicant
 router.post('/v7/select-your-address-applicant', function (req, res) {
   res.redirect('confirm-your-address-applicant')
+})
+
+// What is the name of the species
+router.post('/v7/what-is-the-name-of-the-species', function (req, res) {
+  res.redirect('where-did-you-source-your-specimen-from')
 })
 
 // What is your address agent
@@ -93,6 +142,16 @@ router.post('/v7/what-type-of-permit-or-certificate-are-you-applying-for', funct
 // What will you use the certificate for
 router.post('/v7/what-will-you-use-the-certificate-for', function (req, res) {
   res.redirect('are-you-applying-on-behalf-of-someone-else')
+})
+
+// What will you use the permit for
+router.post('/v7/what-will-you-use-your-permit-for', function (req, res) {
+  res.redirect('#')
+})
+
+// Where did you source the specimen from
+router.post('/v7/where-did-you-source-your-specimen-from', function (req, res) {
+  res.redirect('what-will-you-use-your-permit-for')
 })
 
 
