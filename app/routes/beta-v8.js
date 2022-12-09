9eb120b5f3fdb026565d9c54583ab5a8031f8f22
Richard O'Dwyer <richard.odwyer@defra.gov.uk>
2022-12-09 13:04:17 +0000

    Address content and flow updates

--- app/routes/beta-v8.js
+++ app/routes/beta-v8.js
@@ -61,9 +61,9 @@ router.post('/v8/check-your-answers-reuse', function (req, res) {
 })
 
 // Confirm applicants international address
-router.post('/v8/confirm-applicants-international-address', function (req, res) {
-  res.redirect('what-is-the-name-of-the-species')
-})
+// router.post('/v8/confirm-applicants-international-address', function (req, res) {
+//   res.redirect('what-is-the-name-of-the-species')
+// })
 
 // Confirm delivery address
 router.post('/v8/confirm-delivery-address', function (req, res) {
@@ -72,6 +72,28 @@ router.post('/v8/confirm-delivery-address', function (req, res) {
   }
   else
   res.redirect('what-is-the-name-of-the-species')
+})
+
+// Confirm the address
+router.post('/v8/confirm-the-address', function (req, res) {
+  if (req.session.data['fromPage'] == 'cya') {
+  res.redirect('check-your-answers')
+  }
+
+  if (req.session.data['fromPage'] == 'manageApplication') {
+  res.redirect('manage-application')
+  }
+
+  if (req.session.data['fromPage'] == 'manageApplicationNew') {
+  res.redirect('manage-application-new')
+  }
+
+  if (req.session.data['isAgent'] == 'yes') {
+  res.redirect('what-is-your-address-agent-led')
+  }
+  else {
+  res.redirect('what-is-the-name-of-the-species')
+}
 })
 
 // Confirm your address agent
@@ -113,7 +135,7 @@ router.post('/v8/confirm-your-address-agent-led', function (req, res) {
   }
 
   if (req.session.data['delivery-address'] == 'delivery-address-3') {
-      res.redirect('enter-delivery-address')
+      res.redirect('what-is-the-delivery-address')
     }
   })
 
@@ -123,7 +145,14 @@ router.post('/v8/confirm-your-address-applicant', function (req, res) {
   if (req.session.data['delivery-address-applicant'] == 'delivery-address-1-applicant') {
     if (req.session.data['fromPage'] == 'cya') {
     res.redirect('check-your-answers')
+  }
+}
+
+    if (req.session.data['delivery-address-applicant'] == 'delivery-address-2-applicant') {
+      if (req.session.data['fromPage'] == 'cya') {
+      res.redirect('enter-address-manually')
     }
+  }
 
     if (req.session.data['fromPage'] == 'manageApplication') {
     res.redirect('manage-application')
@@ -133,15 +162,15 @@ router.post('/v8/confirm-your-address-applicant', function (req, res) {
     res.redirect('manage-application-new')
     }
 
-  else {
+    if (req.session.data['address-applicant'] == 'address-1-applicant') {
       res.redirect('what-is-the-name-of-the-species')
     }
-  }
 
-if (req.session.data['delivery-address-applicant'] == 'delivery-address-2-applicant') {
-    res.redirect('enter-delivery-address')
-  }
-})
+    if (req.session.data['address-applicant'] == 'address-2-applicant') {
+      res.redirect('what-is-the-delivery-address')
+    }
+
+  })
 
 // Copy application
 router.post('/v8/copy-application', function (req, res) {
@@ -154,13 +183,18 @@ router.post('/v8/declaration', function (req, res) {
 })
 
 // Enter an international address
-router.post('/v8/enter-an-international-address', function (req, res) {
-  res.redirect('confirm-applicants-international-address')
+// router.post('/v8/enter-an-international-address', function (req, res) {
+//   res.redirect('confirm-applicants-international-address')
+// })
+
+// Enter delivery address manually
+router.post('/v8/enter-delivery-address-manually', function (req, res) {
+  res.redirect('confirm-delivery-address')
 })
 
-// Enter delivery address
-router.post('/v8/enter-delivery-address', function (req, res) {
-  res.redirect('confirm-delivery-address')
+// Enter address manually
+router.post('/v8/enter-address-manually', function (req, res) {
+  res.redirect('confirm-the-address')
 })
 
 // Enter your address manually - agent
@@ -174,7 +208,7 @@ router.post('/v8/enter-your-address-manually-agent-led', function (req, res) {
 })
 
 // Enter your address manually - applicant
-router.post('/v7/enter-your-address-manually-applicant', function (req, res) {
+router.post('/v8/enter-your-address-manually-applicant', function (req, res) {
   res.redirect('confirm-your-address-applicant')
 })
 
@@ -265,14 +299,24 @@ else {
 }
 })
 
+// Select address
+router.post('/v8/select-address', function (req, res) {
+  res.redirect('confirm-the-address')
+})
+
+// Select delivery address
+router.post('/v8/select-delivery-address', function (req, res) {
+  res.redirect('confirm-delivery-address')
+})
+
 // Select your address agent
-router.post('/v8/select-your-address-agent-led', function (req, res) {
-  res.redirect('confirm-your-address-agent-led')
+router.post('/v8/select-your-address-agent', function (req, res) {
+  res.redirect('confirm-your-address-agent')
 })
 
 // Select your address agent led
-router.post('/v8/select-your-address-agent', function (req, res) {
-  res.redirect('confirm-your-address-agent')
+router.post('/v8/select-your-address-agent-led', function (req, res) {
+  res.redirect('confirm-your-address-agent-led')
 })
 
 // Select your address applicant
@@ -294,10 +338,16 @@ router.post('/v8/specimen-details', function (req, res) {
   res.redirect('manage-application-new')
   }
 
-else {
+  else {
   res.redirect('permit-details')
 }
 })
+
+// What best describes the specimen
+router.post('/v8/what-best-describes-the-specimen', function (req, res) {
+  res.redirect('trade-term-code')
+})
+
 
 // What is the name of the species
 router.post('/v8/what-is-the-name-of-the-species', function (req, res) {
@@ -312,11 +362,43 @@ router.post('/v8/what-is-the-name-of-the-species', function (req, res) {
   if (req.session.data['fromPage'] == 'manageApplicationNew') {
   res.redirect('manage-application-new')
   }
-else {
+  else {
 
 req.session.data['specimenCount'] = 1
   res.redirect('where-did-you-source-your-specimen-from')
 }
+})
+
+// What is the purpose code
+router.post('/v8/what-is-the-purpose-code', function (req, res) {
+  if (req.session.data['fromPage'] == 'cya') {
+  res.redirect('check-your-answers')
+  }
+
+  if (req.session.data['fromPage'] == 'manageApplication') {
+  res.redirect('manage-application')
+  }
+
+  if (req.session.data['fromPage'] == 'manageApplicationNew') {
+  res.redirect('manage-application-new')
+  }
+
+  if (req.session.data['specimenType'] == 'animal') {
+    if (req.session.data['speciesName'] == 'coral') {
+      res.redirect('trade-term-code') }
+      else {
+      res.redirect('specimen-type')
+      }
+  }
+
+  if (req.session.data['specimenType'] == 'plant') {
+    res.redirect('specimen-type')
+}
+})
+
+// What is the delivery address
+router.post('/v8/what-is-the-delivery-address', function (req, res) {
+  res.redirect('select-delivery-address')
 })
 
 // What is your address agent
