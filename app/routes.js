9312ed59fb8d3090289d6f4a321521ffd2fe275a
Richard O'Dwyer <richard.odwyer@defra.gov.uk>
2023-02-14 15:26:15 +0000

    routes changes

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,7 @@ const router = express.Router()
 router.use('/', require('./routes/beta-v7.js'))
 router.use('/', require('./routes/beta-v8.js'))
 router.use('/', require('./routes/beta-v9.js'))
+router.use('/', require('./routes/beta-v10.js'))
 
 
 module.exports = router
